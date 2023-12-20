fe89ab980b4cc53052fc14c79019b756b717cb4d
cjatoba <claytonjatoba@gmail.com>
2023-12-20 08:26:38 -0300

    finish `/api/v1/status` endpoint

--- test/integrations/api/v1/status/get.test.js
+++ test/integrations/api/v1/status/get.test.js
@@ -1,6 +1,18 @@
 describe("GET /api/v1/status", () => {
-  it("should return status 200", async () => {
+  test("should return status 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
     expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+
+    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+    expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+
+    expect(responseBody.dependencies.database.version).toEqual("16.0");
+    expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+    const openedConnections =
+      responseBody.dependencies.database.opened_connections;
+    expect(openedConnections).toEqual(1);
   });
 });
