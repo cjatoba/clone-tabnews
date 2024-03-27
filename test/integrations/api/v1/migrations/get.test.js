082ee95493793eaf41d1107932dcf0ae7595f9b1
cjatoba <claytonjatoba@gmail.com>
2024-03-27 07:33:41 -0300

    return 201 code in post and update tests

--- test/integrations/api/v1/migrations/get.test.js
+++ test/integrations/api/v1/migrations/get.test.js
@@ -1,12 +1,17 @@
 import database from 'infra/database.js';
 
+async function clearDatabase() {
+  await database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC;");
+}
+
+beforeAll(clearDatabase);
+
 describe("GET /api/v1/migrations", () => {
   test("should return status 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/migrations");
     expect(response.status).toBe(200);
 
     const responseBody = await response.json();
-    console.log(responseBody);
 
     expect(Array.isArray(responseBody)).toBe(true);
     expect(responseBody.length).toBeGreaterThan(0);
