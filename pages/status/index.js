6a20c7149b82a8ba4d6855310039bcd4107a8e06
cjatoba <claytonjatoba@gmail.com>
2024-11-23 10:40:45 -0300

    feat: add initial version of `/status` page

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,14 +1,55 @@
-function CapsLock(props) {
-  const textInCapsLock = props.text.toUpperCase();
+import useSWR from "swr";
 
-  return <p>{textInCapsLock}</p>;
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  return await response.json();
 }
 
 export default function StatusPage() {
   return (
     <>
       <h1>Status</h1>
-      <CapsLock text="test" />
+      <UpdatedAt />
+      <DatabaseStatus />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let updatedAtText = "Carregando...";
+
+  if (!isLoading && data) {
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return <p>Última atualização: {updatedAtText}</p>;
+}
+
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let databaseStatusInformation = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseStatusInformation = (
+      <>
+        <p>Versão: {data.dependencies.database.version}</p>
+        <p>Conexões abertas: {data.dependencies.database.opened_connections}</p>
+        <p>Máximo de conexões: {data.dependencies.database.max_connections}</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h2>Databasse</h2>
+      <div>{databaseStatusInformation}</div>
     </>
   );
 }
