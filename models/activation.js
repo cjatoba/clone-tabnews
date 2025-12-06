b2b0242ebc893a72bec64d01b6015c6cb43c3180
Clayton Jatobá <claytonjatoba@gmail.com>
2025-12-06 06:31:35 -0300

    feat: require `read:activation_token` to access `/api/v1/activations/[token_id]`

--- models/activation.js
+++ models/activation.js
@@ -1,7 +1,8 @@
 import database from "infra/database";
 import email from "infra/email";
-import { NotFoundError } from "infra/errors";
+import { ForbiddenError, NotFoundError } from "infra/errors";
 import webserver from "infra/webserver";
+import authorization from "./authorization";
 import user from "./user";
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000;
@@ -87,10 +88,20 @@ async function markTokenAsUsed(activationTokenId) {
 }
 
 async function activateUserByUserId(userId) {
+  const userToActivate = await user.findOneById(userId);
+
+  if (!authorization.can(userToActivate, "read:activation_token")) {
+    throw new ForbiddenError({
+      message: "Você não pode mais utilizar tokens de ativação.",
+      action: "Entre em contato com o suporte.",
+    });
+  }
+
   const activatedUser = await user.setFeatures(userId, [
     "create:session",
     "read:session",
   ]);
+
   return activatedUser;
 }
 
@@ -114,6 +125,7 @@ const activation = {
   activateUserByUserId,
   findOneValidById,
   sendEmailToUser,
+  EXPIRATION_IN_MILLISECONDS,
 };
 
 export default activation;
